6ed2ca24ceeb71646185e3ba35a5e4d76fa06e3c
totyai <90376621+totyai@users.noreply.github.com>
2025-12-02 09:06:42 +0000

    - Finished project: Guess Number

--- number_guessing.py
+++ number_guessing.py
@@ -14,11 +14,14 @@ logo = """
  :#. .# #:  #      #  #. :#  #. :#           #.   #   #      #         #   #  #:  #  # # #  #   #      #   #    
   :###: :##:#   ###:  :###:  :###:           :##  #   #   ###:         #   #  :##:#  # # #  # ##    ###:   #    
 """
-life, thought = 0
+life = 0
+thought = 0
+correct_answer = False
+
 def init():
     global logo, life, thought
     # TODO - Create user welcome & explain rules
-    print(f"Welcome to\n{logo}.\nI have thought of a number between 1 and 100. You need to guess it under - for easy option 10, for hard option 5 - guesses. ")
+    print(f"Welcome to\n{logo}\nI have thought of a number between 1 and 100. You need to guess it under - for easy option 10, for hard option 5 - guesses. ")
     difficulty = input("Please choose difficulty. Type 'easy' or 'hard': ")
     if difficulty.lower() == "easy":
         life = 10
@@ -30,25 +33,31 @@ def init():
     # TODO - Select the number to guess
     thought = random.choice(range(1,101))
 
+def guessing():
+    global life, thought, correct_answer
+    while not correct_answer and (life > 0):
+        # TODO - Check the guess against the number
+        print(f"You have {life} attempts remaining to guess.")
+        guess = int(input("Make a guess: "))
 
-# TODO - Check the guess against the number
-print(f"You have {life} attempts remaining to guess.")
-guess = input("Make a guess: ")
+        # TODO - Give feedback to the user & manage life
+        if guess > thought:
+            life -= 1
+            print(f"Too high.\nGuess again.")
 
-# TODO - Give feedback to the user & manage life
-if guess > thought:
-    life -= 1
-    print(f"Too high.\nGuess again.\nYou have {life} attempts remaining to guess")
+        elif guess < thought:
+            life -= 1
+            print(f"Too low.\nGuess again.")
 
-elif guess < thought:
-    life -= 1
-    print(f"Too low.\nGuess again\nYou have {life} attempts remaining")
-
-else:
-    print(f"You got it! The answer was {guess}")
+        else:
+            correct_answer = True
+            print(f"You got it! The answer was {guess}.")
 
 def main():
     init()
+    guessing()
+    if life < 1:
+        print("You run out of lifes. You lost.")
 # TODO - build up logic with fucntion declarations etc
 
 if __name__ == "__main__":
